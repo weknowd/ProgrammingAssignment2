bcfbda5af34cbb67b8900eb5eb1ce309d9a432cd
weknowd <weknowd@gmail.com>
2015-01-24 23:23:33 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,58 +1,97 @@
-# Example usage:
-# > x <- matrix(rnorm(16), nrow = 4)          // Create a matrix x
-# > cx <- makeCacheMatrix(x)                  // Create our special matrix
-# > cx$get()                                  // Return the matrix
-# > cacheSolve(cx)                            // Return the inverse
-# > cacheSolve(cx)                            // Call the 2nd time, so return
-#                                             // the cached inverse
-
-
-# makeCacheMatrix: return a list of functions to:
-# 1. Set the value of the matrix
-# 2. Get the value of the matrix
-# 3. Set the value of the inverse
-# 4. Get the value of the inverse
-makeCacheMatrix <- function(x = matrix()) {
-    # inv will store the cached inverse matrix
-    inv <- NULL
-
-    # Setter for the matrix
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    # Getter for the matrix
-    get <- function() x
-
-    # Setter for the inverse
+# makeVector takes a numeric vector, saved in the private variable x
+makeVector <- function(x = numeric()) 
+{
+    # initialize the mean to NULL during the first call to makeVector
+    # this is needed because ig getmean() is called immediately after
+    # the makeVector funciton is constructed, without a call to setmean
+    # we know we must first calculate the mean in cachemean.  
+    m <- NULL 
     
-    setinv <- function(inverse) inv <<- inverse
-    # Getter for the inverse
-    getinv <- function() inv
+    # funciton to set a new value for the underlying vector
+    # this invalidates the cached mean, m
+    # we use the <<- operator to set the value of x and m because we want 
+    # to modify x and m defined in the enclosing environment (created 
+    # when makeVector was first called), not in the environment local to set(),
+    # in which x and m are undefined.
+    # we must reset m to NULL since we are modifying the underlying
+    # vector and the cached value is no longer the valid 
+    set <- function(y) 
+    {
+        x <<- y
+        m <<- NULL
+    }
 
-    # Return the matrix with our newly defined functions
-    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    # getter function for underlying vector
+    # in R the return value of a function is the last statement.
+    # all of these functions could have been written as:
+    # return(x), etc... as the last line.
+    get <- function()
+    {
+        x
+    }
+
+    # set the mean of the vector x.  Called by cachemean,
+    # this is pretty weird style, but then so is the whole set up.
+    # again we use the <<- operator because we want to modify the m defined
+    # in the enclosing function makeVector, not the m local to setmean,
+    # which would be undefined.
+    setmean <- function(mean) 
+    {
+        m <<- mean
+    }
+    
+    # returns the mean.  Will be null if setmean has not been called or
+    # if set is called after the last call to setmean
+    getmean <- function() 
+    {
+        m
+    }
+
+    # return value of the makeVector function is a list
+    # of functions (and variables if we wish) that we want to expose
+    # as public.  these are accessed with the $ operator.  Any variables
+    # declared inside makeVector but not exported as part of this list
+    # are private...they are inaccessible to any caller of makeVector
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
+# cachemean takes a caching Vector created with makeVector
+cachemean <- function(x, ...) 
+{
+    # get the mean of the vector defined inside x.
+    # we can use the $ operator to access the function since it was
+    # defined in the list of function pointers returned by the call to
+    # makeVector
+    m <- x$getmean()
 
-# cacheSolve: Compute the inverse of the matrix. If the inverse is already
-# calculated before, it returns the cached inverse.
-cacheSolve <- function(x, ...) {
-    inv <- x$getinv()
-
-    # If the inverse is already calculated, return it
-    if (!is.null(inv)) {
+    # if we've already computed the mean and stored it via setmean(),
+    # and have not invalidated the cache by calling set(), return the cached
+    # version of x
+    if(!is.null(m)) 
+    {
         message("getting cached data")
-        return(inv)
+        # we have to explicily use return here otherwise we'd keep
+        # executing the code after the if conditional ends.  Since
+        # the cached version is good, just return it and we are done.
+        return(m)
     }
 
-    # The inverse is not yet calculated, so we calculate it
+    # either we havent computed the cached version yet, or we've called
+    # set() previously and invalidated the cache.
+
+    # call get() to get the underlying vector
     data <- x$get()
-    inv <- solve(data, ...)
 
-    # Cache the inverse
-    x$setinv(inv)
+    # calculate the mean of the underlying vector, passing with it
+    # any varargs passed to cachemean
+    m <- mean(data, ...)
 
-    # Return it
-    inv
+    # now set the mean in x so we cache it and dont need to needlessly
+    # recompute it
+    x$setmean(m)
+    
+    # return the caching vector
+    m
 }
